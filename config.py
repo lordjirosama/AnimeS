5779244d564b46c3beb51937a341bd82ccef0b12
obito96x <uffobitoxe@gmail.com>
2025-07-02 21:58:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ API_HASH = os.environ.get("API_HASH", "4e81464b29d79c58d0ad8a0c55ece4a5")
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002465721727"))
 
 #OWNER ID --⚠️ REQUIRED--
-OWNER_ID = int(os.environ.get("OWNER_ID", "2024483729"))
+OWNER_ID = int(os.environ.get("OWNER_ID", "5585016974"))
 
 #SUPPORT_GROUP: This is used for normal users for getting help if they don't understand how to use the bot --⚠ OPTIONAL--
 SUPPORT_GROUP = os.environ.get("SUPPORT_GROUP", "-1002020461301")
