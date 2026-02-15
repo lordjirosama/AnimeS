e34639240c5685380d2c8ce529fdfc7e314523b0
obito96x <uffobitoxe@gmail.com>
2026-02-15 13:19:48 +0530

    Update advance_cmd.py

--- plugins/advance_cmd.py
+++ plugins/advance_cmd.py
@@ -3,7 +3,7 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQ
 from config import OWNER_ID, START_PIC, START_VIDEO
 from database import kingdb
 
-START_PIC = 
+START_PIC = PICS.Config
 START_VIDEO = START_VIDEO = None  # ya video link = None  # ya video link
 
 
