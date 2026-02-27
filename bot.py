f51d4d269f22d4cec01f4b126948349503314565
akash2316y <iamakashrabha@gmail.com>
2026-02-27 23:10:00 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,9 +12,8 @@ from datetime import datetime
 
 from config import API_HASH, APP_ID, LOGGER, TG_BOT_TOKEN, TG_BOT_WORKERS, CHANNEL_ID, PORT, OWNER_ID
 import pyrogram.utils
+pyrogram.utils.MIN_CHANNEL_ID = -1009147483647
 
-pyrogram.utils.MIN_CHAT_ID = -999999999999
-pyrogram.utils.MIN_CHANNEL_ID = -100999999999999
 
 class Bot(Client):
     def __init__(self):
