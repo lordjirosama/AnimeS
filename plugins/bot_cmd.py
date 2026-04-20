dceed801f03e5b9fa450661646d01c7d1d92d29c
akash2316y <iamakashrabha@gmail.com>
2026-04-20 23:24:40 +0530

    Update bot_cmd.py

--- plugins/bot_cmd.py
+++ plugins/bot_cmd.py
@@ -7,7 +7,7 @@ import time
 from datetime import datetime 
 from pyrogram import Client, filters
 from helper_func import is_admin, get_readable_time, banUser
-from plugins.FORMATS import HELP_TEXT, BAN_TXT, CMD_TXT, USER_CMD_TXT, FSUB_CMD_TXT
+from plugins.FORMATS import HELP_TEXT, BAN_TXT, CMD_TXT, USER_CMD_TXT, FSUB_CMD_TXT, SETTING_TXT
 from pyrogram.errors import FloodWait, UserIsBlocked, InputUserDeactivated
 from database.database import kingdb 
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
