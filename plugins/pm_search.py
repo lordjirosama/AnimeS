a93d4f9a978e5b3a6f34afdf9d8cededd3ea1dc3
akash2316y <iamakashrabha@gmail.com>
2026-04-23 14:02:35 +0530

    Update pm_search.py

--- plugins/pm_search.py
+++ plugins/pm_search.py
@@ -212,7 +212,7 @@ def build_details_caption(ani_data, clean_title):
             f"〈 {ani_title} 〉\n\n"
             f"🌟{rating}% ⌯ {ani_format} ⍀ {genres}\n"
             f"⋟ Season: {season}\n"
-            f"{episodes}\n"
+            f"{eps_chaps}\n"
             f"⋟ Year: {year}\n"
             f"⋟ Status: {status}\n"
             f"⋟ Quality: 480p, 720p, 1080p\n"
