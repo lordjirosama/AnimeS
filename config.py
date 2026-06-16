5495bb4565e4fab7dda1105ce202674cb19ea382
akash2316y <iamakashrabha@gmail.com>
2026-06-16 21:36:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ load_dotenv()
 TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
 
 #Your API ID from my.telegram.org, --⚠️ REQUIRED--
-APP_ID = int(os.environ.get("APP_ID", ""))
+APP_ID = int(os.environ.get("APP_ID", "0"))
 
 #Your API Hash from my.telegram.org, --⚠️ REQUIRED--
 API_HASH = os.environ.get("API_HASH", "")
