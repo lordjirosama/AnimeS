35a048614c607d08ef118f1870ec6f8d473e4860
akash2316y <iamakashrabha@gmail.com>
2026-06-22 08:51:46 +0530

    Update bot_cmd.py

--- plugins/bot_cmd.py
+++ plugins/bot_cmd.py
@@ -1,7 +1,7 @@
 import os
 import asyncio
 from database import database      # ← apna actual DB import
-users_col = database.users          # ← apna collection name
+users_col = kingdb.user_data          # ← apna collection name
 from asyncio import Lock
 from bot import Bot
 from config import OWNER_ID, SUPPORT_GROUP
