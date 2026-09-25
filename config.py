f36bcfd4eb49d2611496e10350f3920d85205810
lordjirosama <lordjiro615@gmail.com>
2026-09-26 00:57:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -41,7 +41,7 @@ DB_NAME = os.environ.get("DATABASE_NAME", "")
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", ""))
 
 #Collection of pics for Bot // #Optional but atleast one pic link should be replaced if you don't want predefined links
-PICS = (os.environ.get("PICS", "https://ibb.co/sJqz5KZW")).split() #Required
+PICS = (os.environ.get("PICS", "https://i.ibb.co/jktw4G8S/file-30286.jpg")).split() #Required
 VIDEOS = (os.environ.get("VIDEOS", "")).split()
 #set your Custom Caption here, Keep None for Disable Custom Caption
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "")
